52a64a98f945d6f56abc6f013ea40d826e6ac625
Administrator <sayeedofficial6220@gmail.com>
2026-01-04 23:44:40 +0530

    remove /test route

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ app.get("/home", (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
 
-app.get("/test", (req, res) => {
-  console.log("Request received at " + req.url);
-  res.status(200).json({ message: "Server is Up and Running" });
-});
 
 module.exports = app;
