48fec10fea63111316c79ea698c25752cfeef39f
Administrator <sayeedofficial6220@gmail.com>
2026-01-07 23:54:34 +0530

    removed /about /health

--- index.js
+++ index.js
@@ -6,16 +6,4 @@ app.get("/home", (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
 
-
-app.get("/about", (req, res) => {
-  console.log("Request received at " + req.url);
-  res.status(200).json({ message: "About Us" });
-});
-
-
-app.get("/health", (req, res) => {
-  console.log("Request received at " + req.url);
-  res.status(200).json({ message: "Health Check" });
-});
-
 module.exports = app;
