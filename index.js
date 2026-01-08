7bcd8873665943946b84b84cc4b99e83299614b1
Administrator <sayeedofficial6220@gmail.com>
2026-01-08 12:00:00 +0530

    added /help route

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get("/home", (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
 
+
+app.get("/help",(req,res)=>{
+  console.log("Help endpoint accessed at " + req.url);
+  res.status(200).json({ message: "This is the help page." });
+});
+
 module.exports = app;
