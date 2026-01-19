3143b9dcfbb8bb5aed37046dd4ff039663c575af
sayeedofficial <sayeedofficial6220@gmail.com>
2026-01-19 12:00:00 +0530

    added about route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/home", (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
 
+app.get("/about", (req, res) => {
+  console.log("Request received at " + req.url);
+  res.status(200).json({ message: "Hello, About!" });
+});
 
 app.get("/help",(req,res)=>{
   console.log("Help endpoint accessed at " + req.url);
